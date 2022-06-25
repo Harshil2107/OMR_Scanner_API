2d11b2596767b6fb4453b2f1368a74aba0ca9400
Harshil2107 <harshilp2107@gmail.com>
2022-06-25 09:00:14 -0700

    bubles extracted

--- scanner.py
+++ scanner.py
@@ -16,14 +16,14 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (5,5), 0)
 edges = cv2.Canny(blur, 75, 200)
 
-countours = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-countours = imutils.grab_contours(countours)
+contours = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(contours)
 
 page_outline = None
 
-if len(countours) > 0:
-    countours = sorted(countours, key= cv2.contourArea, reverse=True)
-    for i in countours:
+if len(contours) > 0:
+    contours = sorted(contours, key= cv2.contourArea, reverse=True)
+    for i in contours:
         perimeter = cv2.arcLength(i, True)
         approx = cv2.approxPolyDP(i, 0.02 * perimeter, True)
 
@@ -34,7 +34,19 @@ sheet = four_point_transform(image, page_outline.reshape(4, 2))
 wrapped = four_point_transform(gray, page_outline.reshape(4, 2))
 otsu_thresh = cv2.threshold(wrapped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
+contours = cv2.findContours(otsu_thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.imshow('Example - Show image in window', otsu_thresh)
+contours = imutils.grab_contours(contours)
+question_bubble_contours = []
+
+for i in contours:
+    (x,y, w, h) = cv2.boundingRect(i)
+    ar = w/float(h)
+
+    if w >- 20 and h>= 20 and ar>= 0.9 and ar <= 1.1:
+        question_bubble_contours.append(i)
+        cv2.drawContours(sheet, [i], -1, (255,255,0), 2)
+
+cv2.imshow('Example - Show image in window', sheet)
 cv2.waitKey(0)  # waits until a key is pressed
 cv2.destroyAllWindows()
